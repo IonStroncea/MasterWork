527936c19f4465f4e1399a67341ee9878a1c02d3
IonStroncea <strion.ion@gmail.com>
2024-10-22 20:35:50 +0300

    Added csv writer

---
+++ Master/Common/CSVWriter.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Common
+{
+    /// <summary>
+    /// Writes values to csv
+    /// </summary>
+    public class CSVWriter
+    {
+        /// <summary>
+        /// File to write to
+        /// </summary>
+        private string _fileName;
+
+        /// <summary>
+        /// Writer to file
+        /// </summary>
+        private StreamWriter _writer;
+
+        /// <summary>
+        /// Constructor. Sets file name and starts stream writer
+        /// </summary>
+        /// <param name="fileName">File</param>
+        public CSVWriter(string fileName)
+        {
+            _fileName = fileName;
+
+            _writer = new StreamWriter(fileName, false);
+        }
+
+        /// <summary>
+        /// Write line of data
+        /// </summary>
+        /// <param name="values">Values to write</param>
+        public void WriteData(params string[] values)
+        {
+            string lineToWrite = string.Join(",", values);
+
+            _writer.WriteLine(lineToWrite);
+        }
+    }
+}
